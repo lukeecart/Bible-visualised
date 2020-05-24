ceebf2b89c0d3da0846628d461eb9db55eb9e6bb
lukeecart <cartwr40@coventry.ac.uk>
2020-05-24 09:47:28 +0100

    reword main page

--- public/js/search.js
+++ public/js/search.js
@@ -7,100 +7,99 @@ const leftArrow = document.getElementById('left');
 const rightArrow = document.getElementById('right');
 
 function searchDiagram() {
-    var input = document.getElementById("mySearch");
-    if (!input){
-      return;
-    }
-    input.focus();
-  
-    myDiagram.startTransaction("highlight search");
-  
-    if (input.value) {
-      var regex = new RegExp(input.value[0].toUpperCase() +  
-      input.value.toLowerCase().slice(1));
-      var results = myDiagram.findNodesByExample({ "name": regex }, {"Also Called": regex});
-      // try to center the diagram at the first node that was found
-      myDiagram.scale = 1;
-      if (results.count >= 1){
-        myDiagram.highlightCollection(results); 
-        myDiagram.centerRect(results.first().actualBounds);
-        FullResults = [];
-        resultIndex = 0;
-        FullResults.push(results.first())
-        while (results.next()) {
-          var item = results.value;
-          FullResults.push(item)
-        }
-        resultOutput.classList.add('visible')
-        displayArrows(resultIndex, results.count)
-        numOfResults.innerHTML = `${resultIndex+1} of ${results.count}`
-      }
-      else {
-        console.log('NO RESULTS');
-        resultOutput.classList.remove('visible')
-        numOfResults.innerHTML = `No Results`
-      } 
-      // output.innerHTML = `Viewing ${resultIndex+1} of ${FullResults.length}`
-    } else {  // empty string only clears highlighteds collection
-      myDiagram.clearHighlighteds();
-    }
-    myDiagram.commitTransaction("highlight search");
-  
+  var input = document.getElementById("mySearch");
+  if (!input) {
+    return;
   }
+  input.focus();
 
- // Arrows switching through people
-  document.onkeydown = ChangeItem;
-  function ChangeItem(e) {
-    if (FullResults === undefined) return;
-    switch (e.keyCode ) {
-        case 38:
-            if (0 < resultIndex){
-              resultIndex --;
-              displayArrows(resultIndex, FullResults.length)
-              numOfResults.innerHTML = `${resultIndex+1} of ${FullResults.length}`
-              myDiagram.centerRect(FullResults[resultIndex].actualBounds);
-            }
-            break;
-        case 40:
-          if (resultIndex < FullResults.length-1){
-            resultIndex ++;
-            displayArrows(resultIndex, FullResults.length)
-            numOfResults.innerHTML = `${resultIndex+1} of ${FullResults.length}`
-            myDiagram.centerRect(FullResults[resultIndex].actualBounds);
-          }
-            break;
+  myDiagram.startTransaction("highlight search");
+
+  if (input.value) {
+    var regex = new RegExp(input.value, "i");
+    var results = myDiagram.findNodesByExample({ "name": regex }, { "Also Called": regex });
+    // try to center the diagram at the first node that was found
+    myDiagram.scale = 1;
+    if (results.count >= 1) {
+      myDiagram.highlightCollection(results);
+      myDiagram.centerRect(results.first().actualBounds);
+      FullResults = [];
+      resultIndex = 0;
+      FullResults.push(results.first())
+      while (results.next()) {
+        var item = results.value;
+        FullResults.push(item)
+      }
+      resultOutput.classList.add('visible')
+      displayArrows(resultIndex, results.count)
+      numOfResults.innerHTML = `${resultIndex + 1} of ${results.count}`
     }
+    else {
+      console.log('NO RESULTS');
+      resultOutput.classList.remove('visible')
+      numOfResults.innerHTML = `No Results`
+    }
+    // output.innerHTML = `Viewing ${resultIndex+1} of ${FullResults.length}`
+  } else {  // empty string only clears highlighteds collection
+    myDiagram.clearHighlighteds();
+  }
+  myDiagram.commitTransaction("highlight search");
+
+}
+
+// Arrows switching through people
+document.onkeydown = ChangeItem;
+function ChangeItem(e) {
+  if (FullResults === undefined) return;
+  switch (e.keyCode) {
+    case 38:
+      if (0 < resultIndex) {
+        resultIndex--;
+        displayArrows(resultIndex, FullResults.length)
+        numOfResults.innerHTML = `${resultIndex + 1} of ${FullResults.length}`
+        myDiagram.centerRect(FullResults[resultIndex].actualBounds);
+      }
+      break;
+    case 40:
+      if (resultIndex < FullResults.length - 1) {
+        resultIndex++;
+        displayArrows(resultIndex, FullResults.length)
+        numOfResults.innerHTML = `${resultIndex + 1} of ${FullResults.length}`
+        myDiagram.centerRect(FullResults[resultIndex].actualBounds);
+      }
+      break;
+  }
 };
 
 //arrow clicking through people 
 leftArrow.addEventListener('click', () => {
-    resultIndex --;
-    displayArrows(resultIndex, FullResults.length)
-    numOfResults.innerHTML = `${resultIndex+1} of ${FullResults.length}`
-    myDiagram.centerRect(FullResults[resultIndex].actualBounds);
+  resultIndex--;
+  displayArrows(resultIndex, FullResults.length)
+  numOfResults.innerHTML = `${resultIndex + 1} of ${FullResults.length}`
+  myDiagram.centerRect(FullResults[resultIndex].actualBounds);
 })
 
 rightArrow.addEventListener('click', () => {
-  resultIndex ++;
+  resultIndex++;
   displayArrows(resultIndex, FullResults.length)
-  numOfResults.innerHTML = `${resultIndex+1} of ${FullResults.length}`
+  numOfResults.innerHTML = `${resultIndex + 1} of ${FullResults.length}`
   myDiagram.centerRect(FullResults[resultIndex].actualBounds);
 })
 
 //0 or 1 = no arrow, index = 0 = one arrow, index < length -1 = two arrows, index = length-1 = one arrow
-function displayArrows (index, length)  {
+function displayArrows(index, length) {
   console.log(index, length)
-  if (length < 2){
+  if (length < 2) {
     leftArrow.style.display = "none"
     rightArrow.style.display = "none"
     return
-  }else if (index === 0 ){
+  } else if (index === 0) {
     rightArrow.style.display = "flex";
     leftArrow.style.display = "none"
-  }else if ( index !=length-1 ) {
+  } else if (index != length - 1) {
     rightArrow.style.display = "flex";
     leftArrow.style.display = "flex"
-  }else {
+  } else {
     leftArrow.style.display = "flex"
     rightArrow.style.display = "none"
   }
