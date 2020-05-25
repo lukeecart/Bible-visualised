2a0514a1c7c2e008b4d8bcc9664c41f305daafe1
lukeecart <cartwr40@coventry.ac.uk>
2020-05-25 21:08:09 +0100

    added animation to cookie policy

--- public/js/cookieCompliance.js
+++ public/js/cookieCompliance.js
@@ -1,13 +1,16 @@
 const consentButton = document.getElementById('cookieButton');
 const cookiesBar = document.getElementById('myCookieConsent');
+localStorage.removeItem('consent')
+if (!localStorage.consent) {
+    consentButton.addEventListener('click', () => {
+        localStorage.setItem('consent', true);
+        document.head.innerHTML = document.head.innerHTML
+            + '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140445912-2"></script>'
+            + "<script> window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-140445912-2'); </script>"
 
-consentButton.addEventListener('click', () => {
-    localStorage.setItem('consent', true);
-    cookiesBar.className = 'classname';
-})
-
-// const rejectButton = document.getElementById('reject');
-// rejectButton.addEventListener('click', () => {
-//     localStorage.removeItem('consent')
-// })
+        cookiesBar.className = 'slideDown';
+    })
+} else {
+    cookiesBar.style.display = 'none';
+}
 
