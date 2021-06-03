e71062f0d617db758cf8246dc3cd8ced35b6cc4a
Luke Cartwright <lukeecartwright@gmail.com>
2021-06-03 15:05:37 +0100

    make a PWA

---
+++ sw.js
@@ -0,0 +1,52 @@
+const version = '1.0'
+const cacheName = `cache-version-${version}`
+const urls = [
+  '/genealogy.html',
+  '/kings.html',
+  '/projects.html',
+  'https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap',
+  '/styles/baseStyles.css',
+  '/styles/homeStyle.css',
+  '/styles/cookieConsent.css',
+  '/js/overlay.js',
+  '/res/Logo.svg',
+  '/res/Studying.svg',
+  '/res/Favicon.png',
+  '/manifest.json',
+  '/js/peopleCondensed.js',
+]
+
+self.skipWaiting()
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(urls)
+    }),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url).then((response) => {
+        return response || fetch(event.request.url)
+      })
+    }),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheName) => {
+      return Promise.all(
+        cacheName.map((name) => {
+          console.log('deleting old cache')
+          if (name !== cacheName) {
+            return caches.delete(name)
+          }
+        }),
+      )
+    }),
+  )
+})
